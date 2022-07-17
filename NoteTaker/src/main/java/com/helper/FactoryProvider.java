1313becaa4163fcb2e1b90d0a986472a0449edac
omprakash <op4407@gmail.com>
2022-07-17 18:50:56 +0530

    my message that can be altered

--- NoteTaker/src/main/java/com/helper/FactoryProvider.java
+++ NoteTaker/src/main/java/com/helper/FactoryProvider.java
@@ -2,7 +2,7 @@ package com.helper;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
+//add by op for git test
 public class FactoryProvider {
 
 	public static SessionFactory factory;
